52aabcdeb1753483b2c2d7a968e55931baadfe37
Muhammet Konukcu <muhammedknkc@gmail.com>
2025-06-08 15:06:04 +0300

    feat: Android screen view customization

--- composeApp/src/androidMain/kotlin/com/muhammetkonukcu/specifind/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/muhammetkonukcu/specifind/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.muhammetkonukcu.specifind
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.muhammetkonukcu.specifind.screen.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(
+                lightScrim = Color.TRANSPARENT,
+                darkScrim = Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.auto(
+                lightScrim = Color.TRANSPARENT,
+                darkScrim = Color.TRANSPARENT
+            )
+        )
         setContent {
-            App()
+            MainScreen()
         }
     }
 }
@@ -21,5 +32,5 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun AppAndroidPreview() {
-    App()
+    MainScreen()
 }
